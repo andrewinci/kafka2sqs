3c5b127eb49ed133781bc37bb3bcc707ddfd352c
Radu Tutunaru <radu.tutunaru@ovoenergy.com>
2022-09-01 15:32:07 +0100

    removed counts from mtls secrets module

--- modules/mtls_secrets/main.tf
+++ modules/mtls_secrets/main.tf
@@ -15,12 +15,10 @@ resource "aws_secretsmanager_secret_version" "kafka_user_certificate" {
 }
 
 resource "aws_secretsmanager_secret" "kafka_ca_certificate" {
-  count = length(var.ca_certificate) > 0 ? 1 : 0
   name  = "kafka_ca_certificate"
 }
 
 resource "aws_secretsmanager_secret_version" "kafka_ca_certificate" {
-  count     = length(var.ca_certificate) > 0 ? 1 : 0
   secret_id = aws_secretsmanager_secret.kafka_ca_certificate[0].id
   secret_string = jsonencode({
     certificate = var.ca_certificate
@@ -28,12 +26,10 @@ resource "aws_secretsmanager_secret_version" "kafka_ca_certificate" {
 }
 
 resource "aws_secretsmanager_secret" "schema_registry_credentials" {
-  count = length(var.schema_registry_username) > 0 ? 1 : 0
   name  = var.schema_registry_secret_name
 }
 
 resource "aws_secretsmanager_secret_version" "schema_registry_credentials" {
-  count     = length(var.schema_registry_username) > 0 ? 1 : 0
   secret_id = aws_secretsmanager_secret.schema_registry_credentials[0].id
   secret_string = jsonencode({
     username = var.schema_registry_username
